f46cb87e4f83ce62d817678e3c4b995529ee4bc3
Sheenan Tenepre <sheenantenepre@gmail.com>
2017-07-31 22:37:04 +0900

    gulpfile config

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,35 +1,165 @@
-/*
-*  Steadfast
-*  Static site generator
-* */
-
 'use strict';
 
-import path from 'path'
-import gulp from 'gulp'
-import del from 'del'
-import browserSync from 'browser-sync'
-import runSequence from 'run-sequence'
-import gulpLoadPlugins from 'gulp-load-plugins'
+import gulp from 'gulp';
+import del from 'del';
+import runSequence from 'run-sequence';
+import browserSync from 'browser-sync';
+import gulpLoadPlugins from 'gulp-load-plugins';
 
-const $ = gulpLoadPlugins()
+const $ = gulpLoadPlugins();
+const reload = browserSync.reload;
 
-// markup task
+// Lint JavaScript
+gulp.task('lint', () =>
+  gulp.src(['src/scripts/**/*.js','!node_modules/**'])
+    .pipe($.eslint())
+    .pipe($.eslint.format())
+    .pipe($.if(!browserSync.active, $.eslint.failAfterError()))
+);
+
+// Optimize images
+gulp.task('images', () =>
+  gulp.src('src/images/**/*')
+    .pipe($.cache($.imagemin({
+      progressive: true,
+      interlaced: true
+    })))
+    .pipe(gulp.dest('public/images'))
+    .pipe($.size({title: 'images'}))
+);
+
+// Copy all files at the root level (src)
+gulp.task('copy', () =>
+  gulp.src([
+    'src/*',
+    '!src/*.html',
+    'node_modules/apache-server-configs/public/.htaccess'
+  ], {
+    dot: true
+  }).pipe(gulp.dest('public'))
+    .pipe($.size({title: 'copy'}))
+);
+
+// Compile and automatically prefix stylesheets
+gulp.task('styles', () => {
+  const AUTOPREFIXER_BROWSERS = [
+    'ie >= 10',
+    'ie_mob >= 10',
+    'ff >= 30',
+    'chrome >= 34',
+    'safari >= 7',
+    'opera >= 23',
+    'ios >= 7',
+    'android >= 4.4',
+    'bb >= 10'
+  ];
+
+  // For best performance, don't add Sass partials to `gulp.src`
+  return gulp.src([
+    'src/styles/**/*.scss',
+    'src/styles/**/*.css'
+  ])
+    .pipe($.newer('.tmp/styles'))
+    .pipe($.sourcemaps.init())
+    .pipe($.sass({
+      precision: 10
+    }).on('error', $.sass.logError))
+    .pipe($.autoprefixer(AUTOPREFIXER_BROWSERS))
+    .pipe(gulp.dest('.tmp/styles'))
+    // Concatenate and minify styles
+    .pipe($.if('*.css', $.cssnano()))
+    .pipe($.size({title: 'styles'}))
+    .pipe($.sourcemaps.write('./'))
+    .pipe(gulp.dest('public/styles'))
+    .pipe(gulp.dest('.tmp/styles'));
+});
+
+// javascript
+gulp.task('scripts', () =>
+    gulp.src([
+      './src/scripts/main.js'
+    ])
+      .pipe($.newer('.tmp/scripts'))
+      .pipe($.sourcemaps.init())
+      .pipe($.babel())
+      .pipe($.sourcemaps.write())
+      .pipe(gulp.dest('.tmp/scripts'))
+      .pipe($.concat('main.min.js'))
+      .pipe($.uglify({preserveComments: 'some'}))
+      // Output files
+      .pipe($.size({title: 'scripts'}))
+      .pipe($.sourcemaps.write('.'))
+      .pipe(gulp.dest('public/scripts'))
+      .pipe(gulp.dest('.tmp/scripts'))
+);
+
+// Scan your HTML for assets & optimize them
 gulp.task('html', () => {
   return gulp.src('src/**/*.html')
-    .pipe($.hb({
-      partials: './src/html/partials/*.hbs'
+    .pipe($.useref({
+      searchPath: '{.tmp,src}',
+      noAssets: true
     }))
-    .pipe(gulp.dest('./build'))
-})
 
-// default task
-gulp.task('default', cb => {
-  return runSequence(
-    'html',
+    // Minify any HTML
+    .pipe($.if('*.html', $.htmlmin({
+      removeComments: true,
+      collapseWhitespace: true,
+      collapseBooleanAttributes: true,
+      removeAttributeQuotes: true,
+      removeRedundantAttributes: true,
+      removeEmptyAttributes: true,
+      removeScriptTypeAttributes: true,
+      removeStyleLinkTypeAttributes: true,
+      removeOptionalTags: true
+    })))
+    // Output files
+    .pipe($.if('*.html', $.size({title: 'html', showFiles: true})))
+    .pipe(gulp.dest('public'));
+});
+
+// Clean output directory
+gulp.task('clean', () => del(['.tmp', 'public/*', '!public/.git'], {dot: true}));
+
+// default script, development mode
+gulp.task('default', ['scripts', 'styles'], () => {
+  browserSync({
+    notify: false,
+    // Customize the Browsersync console logging prefix
+    logPrefix: 'WSK',
+    // Allow scroll syncing across breakpoints
+    scrollElementMapping: ['main', '.mdl-layout'],
+    server: ['.tmp', 'src'],
+    port: 4200
+  });
+
+  gulp.watch(['src/**/*.html'], reload);
+  gulp.watch(['src/styles/**/*.{scss,css}'], ['styles', reload]);
+  gulp.watch(['src/scripts/**/*.js'], ['lint', 'scripts', reload]);
+  gulp.watch(['src/images/**/*'], reload);
+});
+
+// serve build file
+gulp.task('serve', ['default'], () =>
+  browserSync({
+    notify: false,
+    logPrefix: 'WSK',
+    scrollElementMapping: ['main', '.mdl-layout'],
+    server: 'public',
+    port: 4200
+  })
+);
+
+// Production script
+gulp.task('production', ['clean'], cb =>
+  runSequence(
+    'styles',
+    ['lint', 'html', 'scripts', 'images', 'copy'],
     cb
   )
-})
+);
 
-// production
-// gulp.task('production')
+
+// Load custom tasks from the `tasks` directory
+// Run: `npm install --save-dev require-dir` from the command-line
+// try { require('require-dir')('tasks'); } catch (err) { console.error(err); }
